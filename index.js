0695bd60a65ff5777ffbc245e8ceeee0231605b1
Aman Mangla <amanmangla450@gmail.com>
2026-04-29 15:19:17 +0530

    final test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Aman's First deployed app 🚀")
+    res.send("CI/CD lIVE🚀")
 });
 
 app.listen(3000, () => {
